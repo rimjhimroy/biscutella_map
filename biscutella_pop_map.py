af2d50fe7b0b989fc2eebb8ae124c3eb6306727e
rimjhimroy <rimjhim.roy.ch@gmail.com>
2025-06-13 11:43:55 +0200

    first commit

--- biscutella_pop_map.py
+++ biscutella_pop_map.py
@@ -1,50 +1,39 @@
-import streamlit as st
+mport streamlit as st
 import pandas as pd
+import numpy as np
 import pydeck as pdk
 
-st.title("📍 Biscutella Population Genomics Map")
+st.write("Streamlit has lots of fans in the geo community. 🌍 It supports maps from PyDeck, Folium, Kepler.gl, and others.")
 
-# Load your data
-@st.cache_data
-def load_data():
-    return pd.read_csv("biscutella_pops.csv")  # Replace with your uploaded file name
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
 
-data = load_data()
-
-# Define a tooltip to show sequencing details
-tooltip = {
-    "html": """
-    <b>{population}</b><br/>
-    WGS: {WGS_count}<br/>
-    RADseq: {RADseq_count}<br/>
-    Genome Assembly: {Assembly}
-    """,
-    "style": {
-        "backgroundColor": "navy",
-        "color": "white",
-        "fontSize": "12px"
-    }
-}
-
-# Visual layer
-layer = pdk.Layer(
-    "ScatterplotLayer",
-    data=data,
-    get_position='[lon, lat]',
-    get_radius=25000,
-    get_fill_color='[WGS_count*60, RADseq_count*60, Assembly*255, 180]',
-    pickable=True,
-)
-
-# Show map
 st.pydeck_chart(pdk.Deck(
-    map_style="mapbox://styles/mapbox/light-v9",
+    map_style=None,
     initial_view_state=pdk.ViewState(
-        latitude=data['lat'].mean(),
-        longitude=data['lon'].mean(),
-        zoom=7,
-        pitch=30,
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
     ),
-    layers=[layer],
-    tooltip=tooltip
-))
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))"
